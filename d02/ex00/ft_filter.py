81090c81ffc6dba5647039fa7a7754f7c0442fa8
Vineet PAREKH <vparekh@e1r5p23.42.fr>
2019-11-07 13:24:18 +0100

    added len check

--- d02/ex00/ft_filter.py
+++ d02/ex00/ft_filter.py
@@ -1,6 +1,9 @@
 def ft_filter(function_to_apply, list_of_inputs):
     """ft_filter is implementation for filter function"""
     return_map=[]
+    len_list = len(list_of_inputs)
+    if len_list is 0:
+        return None
     for input in list_of_inputs:
         if (function_to_apply(input)):
             return_map.append(input)
--- d02/ex00/ft_map.py
+++ d02/ex00/ft_map.py
@@ -1,6 +1,9 @@
 def ft_map(function_to_apply, list_of_inputs):
     """ft_map is implementation for map function"""
     return_map=[]
+    len_list = len(list_of_inputs)
+    if len_list is 0:
+        return None
     for input in list_of_inputs:
         return_map.append(function_to_apply(input))
     return return_map
